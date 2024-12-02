1faa6fb953142c83129693b595ca65fd20cc30c7
Šimon Lušňák <lusnak@stud.uniza.sk>
2024-12-02 16:54:54 +0100

    datove modely

--- App/Models/Parking.php
+++ App/Models/Parking.php
@@ -1,6 +1,31 @@
 <?php
 
-class Parking
-{
+namespace App\Models;
 
+use App\Core\Model;
+
+class Parking extends Model {
+    protected ?int $id = null;
+    protected ?int $spot_number = null;
+    protected ?string $status = 'free';
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    public function getSpotNumber(): ?int {
+        return $this->spot_number;
+    }
+
+    public function getStatus(): ?string {
+        return $this->status;
+    }
+
+    public function setSpotNumber(int $spot_number): void {
+        $this->spot_number = $spot_number;
+    }
+
+    public function setStatus(string $status): void {
+        $this->status = $status;
+    }
 }
--- App/Models/Reservation.php
+++ App/Models/Reservation.php
@@ -1,6 +1,58 @@
 <?php
 
-class Reservation
-{
+namespace App\Models;
 
+use App\Core\Model;
+
+class Reservation extends Model {
+    protected ?int $id = null;
+    protected ?string $name = null;
+    protected ?string $email = null;
+    protected ?string $date = null;
+    protected ?string $time = null;
+    protected ?int $spot_id = null;
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    public function getName(): ?string {
+        return $this->name;
+    }
+
+    public function getEmail(): ?string {
+        return $this->email;
+    }
+
+    public function getDate(): ?string {
+        return $this->date;
+    }
+
+    public function getTime(): ?string {
+        return $this->time;
+    }
+
+    public function getSpotId(): ?int {
+        return $this->spot_id;
+    }
+
+    public function setName(string $name): void {
+        $this->name = $name;
+    }
+
+    public function setEmail(string $email): void {
+        $this->email = $email;
+    }
+
+    public function setDate(string $date): void {
+        $this->date = $date;
+    }
+
+    public function setTime(string $time): void {
+        $this->time = $time;
+    }
+
+    public function setSpotId(int $spot_id): void {
+        $this->spot_id = $spot_id;
+    }
 }
