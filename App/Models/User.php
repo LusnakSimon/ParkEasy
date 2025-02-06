fb775dd7fae87ffce82db54a38f89fd9a42d4d0b
Šimon Lušňák <lusnak@stud.uniza.sk>
2025-02-06 15:04:03 +0100

    pridanie modelu User

---
+++ App/Models/User.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Models;
+
+use App\Core\Model;
+
+class User extends Model {
+    protected ?int $id = null;
+    protected string $username;
+    protected string $password;
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    public function getUsername(): string {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): void {
+        $this->username = $username;
+    }
+
+    public function getPassword(): string {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void {
+        $this->password = password_hash($password, PASSWORD_DEFAULT);
+    }
+}
